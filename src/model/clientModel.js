3b0978c15f67a7f2bf701e1bfa42870af573744b
papiqo <nationalite45@gmail.com>
2025-05-27 18:06:34 +0700

    Update

--- src/model/clientModel.js
+++ src/model/clientModel.js
@@ -21,6 +21,7 @@ export const create = async (client) => {
       ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)
     RETURNING *
   `;
+  
   const values = [
     client.client_id,
     client.nama,
