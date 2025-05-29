7191d492406148ea72bb29c6fa0f69cfc72548e7
papiqo <nationalite45@gmail.com>
2025-05-29 12:42:09 +0700

    Update

--- src/service/instaFetchService.js
+++ src/service/instaFetchService.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import pLimit from 'p-limit'; // Tambahan rate limiter
+import pLimit from 'p-limit';
 import * as instaPostModel from '../model/instaPostModel.js';
 import * as instaLikeModel from '../model/instaLikeModel.js';
 import { pool } from '../config/db.js';
@@ -11,7 +11,6 @@ const RAPIDAPI_HOST = 'social-api4.p.rapidapi.com';
 const limit = pLimit(4);
 
 async function getEligibleClients() {
-  // Update nama table dan kolom sesuai real db Anda!
   const res = await pool.query(
     `SELECT client_ID as id, client_insta FROM clients
       WHERE client_status=true AND client_insta_status=true AND client_insta IS NOT NULL`
@@ -24,15 +23,15 @@ export async function fetchAndStoreInstaContent(keys) {
   for (const client of clients) {
     const username = client.client_insta;
 
-    // Rate limited GET posts
+    // --- REQUEST POST (dengan username_or_id_or_url) ---
     const postsRes = await limit(() =>
       axios.get(
-        `https://${RAPIDAPI_HOST}/instagram/user-posts`,
+        `https://${RAPIDAPI_HOST}/v1/posts`,
         {
-          params: { username },
+          params: { username_or_id_or_url: username },
           headers: {
-            'X-RapidAPI-Key': RAPIDAPI_KEY,
-            'X-RapidAPI-Host': RAPIDAPI_HOST,
+            'x-rapidapi-key': RAPIDAPI_KEY,
+            'x-rapidapi-host': RAPIDAPI_HOST,
           },
         }
       )
@@ -45,20 +44,19 @@ export async function fetchAndStoreInstaContent(keys) {
       keys.forEach(k => toSave[k] = post[k]);
       await instaPostModel.upsertInstaPost(toSave);
 
-      // Rate limited GET likes per post
+      // --- REQUEST LIKES (dengan code_or_id_or_url = shortcode) ---
       if (post.shortcode) {
         await limit(async () => {
           const likesRes = await axios.get(
-            `https://${RAPIDAPI_HOST}/instagram/post-likes`,
+            `https://${RAPIDAPI_HOST}/v1/likes`,
             {
-              params: { shortcode: post.shortcode },
+              params: { code_or_id_or_url: post.shortcode },
               headers: {
-                'X-RapidAPI-Key': RAPIDAPI_KEY,
-                'X-RapidAPI-Host': RAPIDAPI_HOST,
+                'x-rapidapi-key': RAPIDAPI_KEY,
+                'x-rapidapi-host': RAPIDAPI_HOST,
               },
             }
           );
-          // Simpan hanya array username likes
           const likesUsernames = (likesRes.data.data || []).map(like => like.username);
           await instaLikeModel.upsertInstaLike(post.shortcode, likesUsernames);
         });
