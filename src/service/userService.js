e067a0127a20082a46a3f2875ede0f659c28c9c1
papiqo <nationalite45@gmail.com>
2025-05-29 15:38:12 +0700

    Update

--- src/service/userService.js
+++ src/service/userService.js
@@ -102,7 +102,7 @@ export async function findUserByField(field, value) {
 export async function getUsersByClient(client_id) {
   // Pastikan nama tabel dan field sesuai di database!
   const res = await pool.query(
-    `SELECT user_id, nama, insta FROM users WHERE client_id = $1 AND insta IS NOT NULL AND insta != ''`,
+    `SELECT user_id, nama, insta FROM user WHERE client_id = $1 AND insta IS NOT NULL AND insta != ''`,
     [client_id]
   );
   return res.rows;
