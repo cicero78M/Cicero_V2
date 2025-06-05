ebe57f3f77b2c1194791167499f2e79626914c3b
papiqo <nationalite45@gmail.com>
2025-06-05 21:46:07 +0700

    Update

--- src/cron/cronTiktokService.js
+++ src/cron/cronTiktokService.js
@@ -1,7 +1,7 @@
 import cron from "node-cron";
 import { cronTiktokHarian } from "../service/cronTiktokService.js";
 
-cron.schedule("0 7 * * *", async () => {
+cron.schedule("50 6-22 * * *", async () => {
   try {
     // broadcast ke admin/nomor WA tertentu, misal group admin
     const chatId = "6281234567890@c.us"; // Ganti dengan nomor admin/tujuan WA
