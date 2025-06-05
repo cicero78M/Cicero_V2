ae957625cea34b3b33bf3424f76c27e5c3ce9c3b
papiqo <nationalite45@gmail.com>
2025-06-05 22:05:52 +0700

    Update

--- src/service/waService.js
+++ src/service/waService.js
@@ -28,7 +28,7 @@ import { getUsersByClient } from "../model/userModel.js";
 import { userMenuHandlers } from "../handler/menu/userMenuHandlers.js";
 import { clientRequestHandlers } from "../handler/menu/clientRequestHandlers.js";
 import { oprRequestHandlers } from "../handler/menu/oprRequestHandlers.js";
-import { handleAbsensiKomentar } from "../handler/fetchAbsensi/absensiKomentarHandlers.js";
+import { handleAbsensiKomentar } from "../handler/fetchAbsensi/tiktok/absenisKomentarTiktok.js";
 
 // helper functions
 import {
