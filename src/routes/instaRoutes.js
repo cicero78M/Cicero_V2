61f59a4308fe4d2aeca4a1451dcdce48acdb4437
rizqa febryan prastyo <47109047+cicero78M@users.noreply.github.com>
2025-06-11 20:04:57 +0700

    feat: add rapid instagram posts endpoint

--- src/routes/instaRoutes.js
+++ src/routes/instaRoutes.js
@@ -1,11 +1,12 @@
 // src/routes/instaRoutes.js
 import { Router } from "express";
-import { getInstaRekapLikes, getInstaPosts } from "../controller/instaController.js";
+import { getInstaRekapLikes, getInstaPosts, getRapidInstagramPosts } from "../controller/instaController.js";
 import { authRequired } from "../middleware/authMiddleware.js"; // tambahkan import ini
 
 const router = Router();
 
 router.get("/rekap-likes", authRequired, getInstaRekapLikes);
 router.get("/posts", authRequired, getInstaPosts);
+router.get("/rapid-posts", authRequired, getRapidInstagramPosts);
 
 export default router;
