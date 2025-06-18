af92e385da781ac4b30aa7ce7989f51bbbd69a04
rizqa febryan prastyo <47109047+cicero78M@users.noreply.github.com>
2025-06-18 20:38:06 +0700

    Require JWT_SECRET

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -5,7 +5,7 @@ export function authRequired(req, res, next) {
   const token = req.cookies?.token || req.headers.authorization?.split(' ')[1];
   if (!token) return res.status(401).json({ success: false, message: 'Token required' });
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET || "secretkey");
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
   } catch (err) {
