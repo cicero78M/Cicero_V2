32b00fd1e2a90a01d14f33a9771c584998b49fc3
papiqo <nationalite45@gmail.com>
2025-06-18 23:39:08 +0700

    Update

--- src/cron/cronInstaDataMining.js
+++ src/cron/cronInstaDataMining.js
@@ -19,7 +19,7 @@ async function getActiveClientsIG() {
 
 
 cron.schedule(
-  '24 23 * * *',
+  '40 23 * * *',
   async () => {
     sendDebug({ tag: 'IG DM', msg: 'Mulai cron data mining Instagram' });
     try {
