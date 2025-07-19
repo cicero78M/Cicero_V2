9acec862e1491191b90cae8e57288a451814cf7a
rizqa febryan prastyo <47109047+cicero78M@users.noreply.github.com>
2025-07-19 16:35:00 +0700

    Remove unused TikTok comment rekap service

--- src/service/tiktokCommentService.js
+++ src/service/tiktokCommentService.js
@@ -4,9 +4,3 @@ export const findByVideoId = async (video_id) => {
   return await tiktokCommentModel.findByVideoId(video_id);
 };
 
-
-
-export const getRekapByClient = async (client_id, periode = 'harian') => {
-  return await tiktokCommentModel.getRekapKomentarByClient(client_id, periode);
-};
-
