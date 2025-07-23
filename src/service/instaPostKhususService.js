0fa141a2c163c6f79b47af0788e8a5674fbef766
rizqa febryan prastyo <47109047+cicero78M@users.noreply.github.com>
2025-07-23 10:12:17 +0700

    Limit special tasks endpoint to today's posts

--- src/service/instaPostKhususService.js
+++ src/service/instaPostKhususService.js
@@ -3,3 +3,7 @@ import * as instaPostKhususModel from '../model/instaPostKhususModel.js';
 export const findByClientId = async (client_id) => {
   return await instaPostKhususModel.findByClientId(client_id);
 };
+
+export const findTodayByClientId = async (client_id) => {
+  return await instaPostKhususModel.getPostsTodayByClient(client_id);
+};
