7d0ddce5284992f2d5906fc56efa8fd4ea0d8a36
rizqa febryan prastyo <47109047+cicero78M@users.noreply.github.com>
2025-08-10 22:46:20 +0700

    Test username normalization in like attendance

---
+++ tests/absensiLikesInsta.test.js
@@ -0,0 +1,46 @@
+import { jest } from '@jest/globals';
+
+const mockQuery = jest.fn();
+const mockGetUsersByClient = jest.fn();
+const mockGetShortcodesTodayByClient = jest.fn();
+const mockGetLikesByShortcode = jest.fn();
+
+jest.unstable_mockModule('../src/db/index.js', () => ({ query: mockQuery }));
+jest.unstable_mockModule('../src/model/userModel.js', () => ({ getUsersByClient: mockGetUsersByClient }));
+jest.unstable_mockModule('../src/model/instaPostModel.js', () => ({ getShortcodesTodayByClient: mockGetShortcodesTodayByClient }));
+jest.unstable_mockModule('../src/model/instaLikeModel.js', () => ({ getLikesByShortcode: mockGetLikesByShortcode }));
+
+let absensiLikes;
+
+beforeAll(async () => {
+  ({ absensiLikes } = await import('../src/handler/fetchabsensi/insta/absensiLikesInsta.js'));
+});
+
+beforeEach(() => {
+  mockQuery.mockReset();
+  mockGetUsersByClient.mockReset();
+  mockGetShortcodesTodayByClient.mockReset();
+  mockGetLikesByShortcode.mockReset();
+});
+
+test('marks user with @username as already liking', async () => {
+  mockQuery.mockResolvedValueOnce({ rows: [{ nama: 'POLRES ABC' }] });
+  mockGetUsersByClient.mockResolvedValueOnce([
+    {
+      user_id: 'u1',
+      title: 'Aiptu',
+      nama: 'Budi',
+      insta: '@TestUser',
+      divisi: 'BAG',
+      exception: false,
+    },
+  ]);
+  mockGetShortcodesTodayByClient.mockResolvedValueOnce(['sc1']);
+  mockGetLikesByShortcode.mockResolvedValueOnce(['testuser']);
+
+  const msg = await absensiLikes('POLRES', { mode: 'sudah' });
+
+  expect(msg).toMatch(/Sudah melaksanakan\* : \*1 user\*/);
+  expect(msg).toMatch(/Belum melaksanakan\* : \*0 user\*/);
+});
+
