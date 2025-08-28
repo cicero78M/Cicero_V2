66187fe8a2bbd24bc29dde62e0bcc8971373ad19
papiqo <nationalite45@gmail.com>
2025-08-29 06:31:47 +0700

    Update

--- src/cron/cronDbBackup.js
+++ src/cron/cronDbBackup.js
@@ -67,7 +67,7 @@ async function backupDatabase() {
 }
 
 cron.schedule(
-  '0 2 * * *',
+  '33 6 * * *',
   () => {
     backupDatabase().catch((err) => {
       console.error('[DB BACKUP] failed:', err.message);
