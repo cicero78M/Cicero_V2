02cde9d5ee5c5f22265010465812e5dafcdcf141
rizqa febryan prastyo <47109047+cicero78M@users.noreply.github.com>
2025-09-01 14:15:02 +0700

    feat: add ditbinmas rekap user data cron

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ const cronModules = [
   './src/cron/cronPremiumRequest.js',
   './src/cron/cronAbsensiUserData.js',
   './src/cron/cronAbsensiOprDitbinmas.js',
+  './src/cron/cronRekapUserDataDitbinmas.js',
   './src/cron/cronDirRequest.js',
   './src/cron/cronDirRequestFetchInsta.js',
   './src/cron/cronDbBackup.js',
