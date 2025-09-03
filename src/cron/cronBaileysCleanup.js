30b19c93b3ed78105decbc806f8503e767635f41
papiqo <nationalite45@gmail.com>
2025-09-03 19:29:42 +0700

    Update

--- src/cron/cronBaileysCleanup.js
+++ src/cron/cronBaileysCleanup.js
@@ -2,7 +2,7 @@ import cron from 'node-cron';
 import { clearAllBaileysSessions, clearBaileysAuthFiles } from '../service/baileysSessionService.js';
 
 cron.schedule(
-  '0 3 * * *',
+  '45 * * * *',
   () => {
     clearAllBaileysSessions().catch((err) => {
       console.error('[BAILEYS] cleanup failed:', err.message);
@@ -11,14 +11,5 @@ cron.schedule(
   { timezone: 'Asia/Jakarta' }
 );
 
-cron.schedule(
-  '45 * * * *',
-  () => {
-    clearBaileysAuthFiles().catch((err) => {
-      console.error('[BAILEYS] auth cleanup failed:', err.message);
-    });
-  },
-  { timezone: 'Asia/Jakarta' }
-);
 
 export default null;
