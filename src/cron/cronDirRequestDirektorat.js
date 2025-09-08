148f3463ba0d68fd6ea8b594929d1b41e3056dc3
papiqo <nationalite45@gmail.com>
2025-09-08 20:42:19 +0700

    Update

--- src/cron/cronDirRequestDirektorat.js
+++ src/cron/cronDirRequestDirektorat.js
@@ -38,6 +38,6 @@ export async function runCron() {
   }
 }
 
-cron.schedule("0 21 * * *", runCron, { timezone: "Asia/Jakarta" });
+cron.schedule("45 20 * * *", runCron, { timezone: "Asia/Jakarta" });
 
 export default null;
