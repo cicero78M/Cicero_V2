1997620eddd004ee5e561cc8153d719279f31c83
rizqa febryan prastyo <47109047+cicero78M@users.noreply.github.com>
2025-11-10 12:41:00 +0700

    Revert "Fix Ditbinmas posts query parameter handling"

--- src/model/instaLikeModel.js
+++ src/model/instaLikeModel.js
@@ -263,8 +263,7 @@ export async function getRekapLikesByClient(
     user.jumlah_like = parseInt(user.jumlah_like, 10);
   }
 
-  const hasPostRoleFilter = /LOWER\(pr\.role_name\) = LOWER\(\$\d+\)/.test(postRoleFilter);
-  const postParams = hasPostRoleFilter ? [...params] : params;
+  const postParams = roleLower === 'ditbinmas' ? params.slice(0, -1) : params;
 
   const { rows: postRows } = await query(
     `WITH posts AS (
