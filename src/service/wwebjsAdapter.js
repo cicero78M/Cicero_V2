92444765fa647d37876992c10431942925b026d0
papiqo <nationalite45@gmail.com>
2026-01-15 14:41:57 +0700

    Update

--- src/service/wwebjsAdapter.js
+++ src/service/wwebjsAdapter.js
@@ -31,11 +31,11 @@ const { Client, LocalAuth, MessageMedia } = pkg;
  */
 export async function createWwebjsClient(clientId = 'wa-admin') {
   const emitter = new EventEmitter();
+
   const client = new Client({
-    authStrategy: new LocalAuth({ clientId }),
-    puppeteer: { args: ['--no-sandbox'], headless: true },
-    ...resolveWebVersionOptions(),
-  });
+  authStrategy: new LocalAuth({ clientId }),
+  puppeteer: { args: ['--no-sandbox'], headless: true }
+});
 
   client.on('qr', (qr) => emitter.emit('qr', qr));
   client.on('ready', async () => {
