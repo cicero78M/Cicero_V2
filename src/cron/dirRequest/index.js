757ff1f05eac3b69e1e2e15528550e1151db1090
papiqo <nationalite45@gmail.com>
2026-01-22 06:35:26 +0700

    Update

--- src/cron/dirRequest/index.js
+++ src/cron/dirRequest/index.js
@@ -136,7 +136,7 @@ const dirRequestCrons = [
   {
     jobKey: DIRREQUEST_CUSTOM_SEQUENCE_JOB_KEY,
     description:
-      'Run dirRequest custom sequence: sosmed fetch, Ditsamapta menus 6/9/28/29 (plus extras), Ditbinmas combined recap (menu 21), then Bidhumas menus 6/9/28/29 to group + super admin.',
+      'Run dirRequest custom sequence: sosmed fetch, Ditbinmas combined recap (menu 21), then Bidhumas menus 6/9/28/29 to group + super admin.',
     schedules: [
       { cronExpression: '0 15 * * *', handler: () => runDirRequestCustomSequence(), options: DEFAULT_CRON_OPTIONS },
       { cronExpression: '05 18 * * *', handler: () => runDirRequestCustomSequence(), options: DEFAULT_CRON_OPTIONS },
